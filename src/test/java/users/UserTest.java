61d67b1189a3dfa12eaea4544e18ad385f51b886
Dmitrii Bogoslovskii <vrubel@labma.ru>
2025-03-09 02:45:20 +0300

    Added UserTest

---
+++ src/test/java/users/UserTest.java
@@ -0,0 +1,81 @@
+package users;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.petstore.api.store.StoreService;
+import org.petstore.api.user.UserService;
+import org.petstore.config.rest.Config;
+import org.petstore.models.pet.Pet;
+import org.petstore.models.user.User;
+import org.petstore.util.factory.pet.PetFactory;
+import org.petstore.util.factory.user.UserFactory;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class UserTest {
+
+    private static UserService users;
+
+    @BeforeAll
+    static void setup() {
+        Config.setup();
+        users = new UserService();
+    }
+
+    @Test
+    void testCreateAndGetUser() {
+        User user = UserFactory.generateUser();
+
+        users.createUser(user)
+                .then()
+                .statusCode(200);
+
+        User responseUser = users.getUserByName(user.getUsername())
+                .then()
+                .statusCode(200)
+                .extract().as(User.class);
+
+        assertThat(responseUser.getUsername(), equalTo(user.getUsername()));
+        assertThat(responseUser.getEmail(), equalTo(user.getEmail()));
+    }
+
+    @Test
+    void testUpdateUser() {
+        User user = UserFactory.generateUser();
+        users.createUser(user)
+                .then()
+                .statusCode(200);
+
+        user.setFirstName("UpdatedName");
+        user.setLastName("UpdatedLastName");
+
+        users.updateUser(user.getUsername(), user)
+                .then()
+                .statusCode(200);
+
+        User updatedUser = users.getUserByName(user.getUsername())
+                .then()
+                .statusCode(200)
+                .extract().as(User.class);
+
+        assertThat(updatedUser.getFirstName(), equalTo("UpdatedName"));
+        assertThat(updatedUser.getLastName(), equalTo("UpdatedLastName"));
+    }
+
+    @Test
+    void testDeleteUser() {
+        User user = UserFactory.generateUser();
+        users.createUser(user)
+                .then()
+                .statusCode(200);
+
+        users.deleteUser(user.getUsername())
+                .then()
+                .statusCode(200);
+
+        users.getUserByName(user.getUsername())
+                .then()
+                .statusCode(404);
+    }
+}
